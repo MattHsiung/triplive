03d47150a3ac4b9b4bbe4a68da183adbb52e3270
Matt Hsiung <xung@musician.com>
2016-02-04 11:15:33 -0500

    3d BANANAS

--- public/js/control.js
+++ public/js/control.js
@@ -186,4 +186,29 @@ $(document).ready(function() {
 	  removeEvent('restaurant')
 	  removeEvent('activity')
 });
-console.log(hotels)
+//---------
+var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+			var renderer = new THREE.WebGLRenderer({alpha:true});
+			renderer.setSize( window.innerWidth/8, window.innerHeight/8 );
+			$('#control-panel').append( renderer.domElement );
+
+			var texture = THREE.ImageUtils.loadTexture( "/images/1.gif" );
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 ).scale(4,4,4);
+			var material = new THREE.MeshBasicMaterial( { map:texture} )
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			var render = function () {
+				requestAnimationFrame( render );
+
+				cube.rotation.x += 0.05;
+				cube.rotation.y += 0.05;
+
+				renderer.render(scene, camera);
+			};
+
+			render();
